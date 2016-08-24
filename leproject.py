ebcb5033c7f82aa14c7f6f751501e7dc99dc867a
Dokuro <Dokuro>
2016-08-24 14:05:47 +0200

    classes

--- leproject.py
+++ leproject.py
@@ -5,50 +5,65 @@ import time
 import json
 import os
 import psycopg2
-import asis
-
 from collections import OrderedDict
 
-with open("settings", "r") as yamlset:
-    settings=yaml.load(yamlset)
+class main:
+    joutput = "output"
+    toutput = "outputtext"
+    dboutput = "prog1"
+    data = OrderedDict()
+    def __init__(self):
+        with open("settings", "r") as yamlset:
+            self.settings=yaml.load(yamlset)
 
-def monitor(number, output, interval):
-    print(output)
-    data=OrderedDict()
-    ts=time.time()
-    data["snapshot"]="Snapshot"+str(number)
-    data["timestamp"]=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    data["cpu"]=psutil.cpu_times()
-    data["virtual_memory"]=psutil.virtual_memory()
-    data["disk_usage"]=psutil.disk_usage('/')
-    data["IO"]=psutil.disk_io_counters()
-    data["network"]=psutil.net_io_counters()
-    try:
-        if output=="db":
-            con = psycopg2.connect("dbname='pytest' user='root'")
-            cur = con.cursor()
-            cols = data.keys()
-            vals = [data[x] for x in cols]
-            vals_str_list = ["%s"] * len(vals)
-            vals_str = ", ".join(vals_str_list)
-            cur.execute("INSERT INTO prog1 VALUES ({vals_str})".format(vals_str=vals_str), vals)
-            con.commit()
-        if output=="json":
-            with open("output", "a") as jf:
-                json.dump(data, jf, indent=3)
-                jf.write(os.linesep)
-        elif output=="text":
-            with open("outputtext", "a") as tf:
-                for key, value in data.items():
-                    tf.write('%s:%s\n' % (key, value))
-    except:
-        print("wrong output format!")
+    def display_init(self):
+        print("initial setting: %s" % self.settings)
 
-def run():
-    number = 1
-    while True:
-        time.sleep(settings["interval"])
-        monitor(number, **settings)
-        number+=1
+class monitor(main):
+    def monitor(number):
+        data=main.data
+        ts = time.time()
+        data["snapshot"]="Snapshot"+str(number)
+        data["timestamp"]=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        data["cpu"]=psutil.cpu_times()
+        data["virtual_memory"]=psutil.virtual_memory()
+        data["disk_usage"]=psutil.disk_usage('/')
+        data["IO"]=psutil.disk_io_counters()
+        data["network"]=psutil.net_io_counters()
 
-run()
+class output(monitor):
+    def output(output):
+        print(output)
+        try:
+            if output=="db":
+                con = psycopg2.connect("dbname='pytest' user='root'")
+                cur = con.cursor()
+                cols = monitor.data.keys()
+                vals = [monitor.data[x] for x in cols]
+                vals_str_list = ["%s"] * len(vals)
+                vals_str = ", ".join(vals_str_list)
+                cur.execute("INSERT INTO "+main.dboutput+" VALUES ({vals_str})".format(vals_str=vals_str), vals)
+                con.commit()
+            if output=="json":
+                with open(main.joutput, "a") as jf:
+                    json.dump(monitor.data, jf, indent=3)
+                    jf.write(os.linesep)
+            elif output=="text":
+                with open(main.toutput, "a") as tf:
+                    for key, value in monitor.data.items():
+                        tf.write('%s:%s\n' % (key, value))
+        except:
+            print("wrong output format!")
+
+class run(output):
+    def run(self):
+        number = 1
+        while True:
+            time.sleep(self.settings["interval"])
+            output.monitor(number)
+            output.output(self.settings["output"])
+            number+=1
+
+obj=run()
+obj.display_init()
+obj.run()
